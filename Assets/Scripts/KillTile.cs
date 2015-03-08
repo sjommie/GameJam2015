1e56572b373b1f2a6a8fb4fb9cf06e482b82e487
tommydeboer <tommydeboer4@gmail.com>
2015-03-08 15:21:07 +0100

    Added KillTile which can be added to tiles that can kill you, like your mother

---
+++ Assets/Scripts/KillTile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTile : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			// TODO: Add Game Controller.triggerPickup
+			//GameController.setDead (other);
+		}
+	}
+
+}
